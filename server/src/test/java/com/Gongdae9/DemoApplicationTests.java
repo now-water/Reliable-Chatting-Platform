9dbb29f6122b1a2123b09127ecc29f9b22e42465
kcm0147 <kcm0147@hanmail.net>
2021-04-01 15:50:47 +0900

    establish: spring boot setting

---
+++ server/src/test/java/com/Gongdae9/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gongdae9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
