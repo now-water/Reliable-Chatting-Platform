3ee70e8bd3cc5fb8638ab2a6ec4ada51a81710c0
kcm0147 <kcm0147@hanmail.net>
2021-04-01 16:48:43 +0900

    feat: joinRoom 연관관계 편의메소드 정의

--- server/src/main/java/com/Gongdae9/domain/JoinRoom.java
+++ server/src/main/java/com/Gongdae9/domain/JoinRoom.java
@@ -26,4 +26,20 @@ public class JoinRoom {
     @JoinColumn(name="room_id")
     private Room room;
 
+    // 연관관계 편의 메소드
+    public void setUser(User user){
+        if(this.user!=null){
+            this.user.getJoinRooms().remove(this);
+        }
+        this.user=user;
+        user.getJoinRooms().add(this);
+    }
+
+    public void setRoom(Room room){
+        if(this.room!=null){
+            this.room.getJoinRooms().remove(this);
+        }
+        this.room=room;
+        room.getJoinRooms().add(this);
+    }
 }
