355a71d466e753b2e6f338dbd4c839dc44412880
o-star <uh04049@naver.com>
2021-04-02 10:39:50 +0900

    main activity and usrlist layout frame add

--- client/app/src/main/java/com/example/chattingapp/view/MainActivity.kt
+++ client/app/src/main/java/com/example/chattingapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.chattingapp
+package com.example.chattingapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chattingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
