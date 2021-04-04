af088136ad6108a26e5af45fc1595950155ec507
o-star <uh04049@naver.com>
2021-04-04 13:04:06 +0900

    recycler view add and fragment transaction update

--- client/app/src/main/java/com/example/chattingapp/view/MainActivity.kt
+++ client/app/src/main/java/com/example/chattingapp/view/MainActivity.kt
@@ -4,21 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.chattingapp.R
+import com.example.chattingapp.view.fragment.SettingFragment
+import com.example.chattingapp.view.fragment.UserlistFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("MSG","TEST")
+        Log.e("MSG", "TEST")
         setContentView(R.layout.activity_main)
 
         setFrag(0)
+
+        btn_userlist.setOnClickListener {
+            setFrag(0)
+        }
+
+        btn_chatlist.setOnClickListener {
+            setFrag(1)
+        }
+
+        btn_setting.setOnClickListener {
+            setFrag(2)
+        }
+
     }
 
     //test code for fragment visible
     private fun setFrag(fragNum: Int) {
         val ft = supportFragmentManager.beginTransaction()
-        when(fragNum){
+        when (fragNum) {
             0 -> {
+                ft.replace(R.id.main_frame, UserlistFragment()).commit()
+            }
+            2 -> {
                 ft.replace(R.id.main_frame, SettingFragment()).commit()
             }
         }
--- client/app/src/main/java/com/example/chattingapp/view/fragment/SettingFragment.kt
+++ client/app/src/main/java/com/example/chattingapp/view/fragment/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.chattingapp.view
+package com.example.chattingapp.view.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
