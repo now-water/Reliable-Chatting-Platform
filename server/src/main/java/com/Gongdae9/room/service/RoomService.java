8cc8cd59b27fe1edf6a89248ff9d019cc4338830
kcm0147 <kcm0147@hanmail.net>
2021-04-27 00:57:18 +0900

    feat: inviteFriend

--- server/src/main/java/com/Gongdae9/room/service/RoomService.java
+++ server/src/main/java/com/Gongdae9/room/service/RoomService.java
@@ -3,6 +3,7 @@ package com.Gongdae9.room.service;
 
 import com.Gongdae9.room.domain.Room;
 import com.Gongdae9.room.repository.RoomRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +16,8 @@ public class RoomService {
     public boolean createRoom(Room room){
         return roomRepository.save(room);
     }
+
+    public Room findById(Long id){ return roomRepository.findById(id);}
+
+    public List<Room> findAll(){return roomRepository.findAll();}
 }
