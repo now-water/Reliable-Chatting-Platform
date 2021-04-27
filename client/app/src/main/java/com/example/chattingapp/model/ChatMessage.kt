ebbc4e96e99c3a947ec2cb2c9152e22ef0847c53
hyodonglee <good79797979@naver.com>
2021-04-27 19:30:26 +0900

    chore : delete Model directory

--- client/app/src/main/java/com/example/chattingapp/model/ChatMessage.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chattingapp.model
-
-class ChatMessage(val roomId: Long, val userId: Long?, val bookmarkId: Long?,
-                  val content: String?, val unreadCnt: Int, val writtenBy: String) {
-
-
-}
