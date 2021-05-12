96864e74710dac24ad95ea164a964936f0bcb09f
o-star <uh04049@naver.com>
2021-05-12 20:34:35 +0900

    bookmark view update

--- client/app/src/main/java/com/example/chattingapp/view/BookmarkActivity.kt
+++ client/app/src/main/java/com/example/chattingapp/view/BookmarkActivity.kt
@@ -2,13 +2,13 @@ package com.example.chattingapp.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chattingapp.R
 
 class BookmarkActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmark)
     }
 }
