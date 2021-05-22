fdc49d81ded49ad9a6608f29814d29106440cc3e
hyodonglee <good79797979@naver.com>
2021-05-23 02:30:23 +0900

    feat : search friend

--- client/app/src/main/java/com/example/chattingapp/adapter/FriendlistAdapter.kt
+++ client/app/src/main/java/com/example/chattingapp/adapter/FriendlistAdapter.kt
@@ -3,6 +3,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chattingapp.R
@@ -10,8 +12,8 @@ import com.example.chattingapp.dto.Friend
 
 // Main Userlist type Adapter
 class FriendlistAdapter(val context: Context, var friendList: ArrayList<Friend>) :
-    RecyclerView.Adapter<FriendlistAdapter.Holder>() {
-
+    RecyclerView.Adapter<FriendlistAdapter.Holder>(), Filterable {
+    private var friends: ArrayList<Friend>? = friendList
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         val nameText = itemView?.findViewById<TextView>(R.id.nameText)
@@ -29,15 +31,44 @@ class FriendlistAdapter(val context: Context, var friendList: ArrayList<Friend>)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder?.bind(friendList[position], context)
+        holder?.bind(friends?.get(position)!!, context)
     }
 
     override fun getItemCount(): Int {
-        return friendList.size
+        return friends?.size!!
     }
 
     fun addItem(friend: Friend){
-        friendList.add(friend)
-        notifyItemInserted(friendList.size-1);
+        friends?.add(friend)
+        notifyItemInserted(friends?.size!!-1);
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence): FilterResults {
+                val charString = constraint.toString()
+                friends = if (charString.isEmpty()) {
+                    friendList
+                } else {
+                    val filteredList = ArrayList<Friend>()
+                    if (friendList != null) {
+                        for (friend in friendList) {
+                            if(friend.name.toLowerCase().contains(charString.toLowerCase())) {
+                                filteredList.add(friend);
+                            }
+                        }
+                    }
+                    filteredList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = friends
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence, results: FilterResults) {
+                friends  = results.values as ArrayList<Friend>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
