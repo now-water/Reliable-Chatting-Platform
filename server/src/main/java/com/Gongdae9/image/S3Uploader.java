bce9c4d6e9ad0fb4ea0cb95fd553cdda911320b6
hyun.min.han <um4825@gmail.com>
2021-05-22 17:54:23 +0900

    add temp filepath

--- server/src/main/java/com/Gongdae9/image/S3Uploader.java
+++ server/src/main/java/com/Gongdae9/image/S3Uploader.java
@@ -12,23 +12,22 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Optional;
 
-@Slf4j
-@RequiredArgsConstructor
-@Component
+@Slf4j // 로깅을 위한 어노테이션
+@RequiredArgsConstructor // final 변수에 대한 의존성을 추가합니다.
+@Component // 빈 등록을 위한 어노테이션
 public class S3Uploader {
+
+    private final static String TEMP_FILE_PATH = "src/main/resources/";
 
     private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    @Value("${cloud.aws.s3.bucket}")  // 프로퍼티에서 cloude.aws.s3.bucket에 대한 정보를 불러옵니다.
+    public String bucket;             // 저는 .properties가 아닌 .yml을 이용하였습니다!
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
-
-        return upload(uploadFile, dirName);
+        File convertedFile = convert(multipartFile);
+        return upload(convertedFile, dirName);
     }
 
     private String upload(File uploadFile, String dirName) {
@@ -45,21 +44,20 @@ public class S3Uploader {
 
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
-            log.info("파일이 삭제되었습니다.");
-        } else {
-            log.info("파일이 삭제되지 못했습니다.");
+            return;
         }
+        log.info("임시 파일이 삭제 되지 못했습니다. 파일 이름: {}", targetFile.getName());
     }
 
-    private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
-        if(convertFile.createNewFile()) {
+    private File convert(MultipartFile file) throws IOException {
+        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
             }
-            return Optional.of(convertFile);
+            return convertFile;
         }
-
-        return Optional.empty();
+        throw new IllegalArgumentException(String.format("파일 변환이 실패했습니다. 파일 이름: %s", file.getName()));
     }
+
 }
