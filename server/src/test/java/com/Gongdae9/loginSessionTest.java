a85bf14aec610762c6c8dfb492b0125f444c07b7
hyun.min.han <um4825@gmail.com>
2021-05-22 17:17:32 +0900

    test annotating

--- server/src/test/java/com/Gongdae9/loginSessionTest.java
+++ server/src/test/java/com/Gongdae9/loginSessionTest.java
@@ -16,41 +16,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class loginSessionTest {
-
-    @Autowired
-    private UserService userService;
-
-//    @Test
-    public void 회원가입_테스트() throws Exception {
-        //given
-        User user = User.builder()
-            .name("테스트 이름4")
-            .phoneNum("테스트 전화번호4")
-            .nickName("테스트 닉네임4")
-            .accountId("testid4")
-            .password("testpw4")
-            .build();
-
-        //when
-        UserDto userDto = userService.signUp(user);
-        Long createdUserId = userDto.getUserId();
-
-        //then
-        Long needCheckId = userService.findById(createdUserId).getUserId();
-        assertThat(createdUserId).isEqualTo(needCheckId);
-    }
-
-//    @Test
-    public void 로그인_테스트() throws Exception {
-        //given
-        LoginRequestDto dto = new LoginRequestDto("testid4", "testpw4", "testToken");
-        MockHttpSession session = new MockHttpSession();
-
-        //when
-        long id = userService.login(dto, session).getUserId();
-        User user = userService.findByAccountId(dto.getAccountId()).get(0);
-
-        //then
-        assertThat(id).isEqualTo(user.getUserId());
-    }
+//
+//    @Autowired
+//    private UserService userService;
+//
+////    @Test
+//    public void 회원가입_테스트() throws Exception {
+//        //given
+//        User user = User.builder()
+//            .name("테스트 이름4")
+//            .phoneNum("테스트 전화번호4")
+//            .nickName("테스트 닉네임4")
+//            .accountId("testid4")
+//            .password("testpw4")
+//            .build();
+//
+//        //when
+//        UserDto userDto = userService.signUp(user);
+//        Long createdUserId = userDto.getUserId();
+//
+//        //then
+//        Long needCheckId = userService.findById(createdUserId).getUserId();
+//        assertThat(createdUserId).isEqualTo(needCheckId);
+//    }
+//
+////    @Test
+//    public void 로그인_테스트() throws Exception {
+//        //given
+//        LoginRequestDto dto = new LoginRequestDto("testid4", "testpw4", "testToken");
+//        MockHttpSession session = new MockHttpSession();
+//
+//        //when
+//        long id = userService.login(dto, session).getUserId();
+//        User user = userService.findByAccountId(dto.getAccountId()).get(0);
+//
+//        //then
+//        assertThat(id).isEqualTo(user.getUserId());
+//    }
 }
