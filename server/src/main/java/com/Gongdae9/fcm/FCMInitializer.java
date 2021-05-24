3f2d274088297f1d404b01fd9e17dc33c76cb27e
hyun.min.han <um4825@gmail.com>
2021-05-25 00:45:32 +0900

    now fcm can file system resource is absolute path

--- server/src/main/java/com/Gongdae9/fcm/FCMInitializer.java
+++ server/src/main/java/com/Gongdae9/fcm/FCMInitializer.java
@@ -3,13 +3,14 @@ package com.Gongdae9.fcm;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.IOException;
-import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 @Service
 public class FCMInitializer {
@@ -23,7 +24,7 @@ public class FCMInitializer {
     public void initialize() {
         try {
             FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream())).build();
+                .setCredentials(GoogleCredentials.fromStream(new FileSystemResource(FIREBASE_CONFIG_PATH).getInputStream())).build();
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
                 logger.info("Firebase application has been initialized");
