652b3aa59bb9b769f1fc936a89a805fd1ec8e72e
Chang Muk Kim <57346393+kcm0147@users.noreply.github.com>
2021-10-04 02:07:13 +0900

    [#229] FEAT: Message Save at DB

--- mqserver/src/main/java/com/rabbitmq/mqserver/message/QueListener.java
+++ mqserver/src/main/java/com/rabbitmq/mqserver/message/QueListener.java
@@ -1,18 +1,33 @@
 package com.rabbitmq.mqserver.message;
 
+import com.rabbitmq.mqserver.message.domain.Message;
+import com.rabbitmq.mqserver.message.dto.MessageDto;
+import com.rabbitmq.mqserver.message.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class QueListener {
 
     private static final Logger log = LoggerFactory.getLogger(QueListener.class);
-
+    private final MessageService messageService;
 
     @RabbitListener(queues = "sample.que")
-    public void receiveMessage(String message) {
-        log.info("que에서 꺼내온 메시지 : {}",message);
+    public void receiveMessage(MessageDto message) {
+
+        log.info("que에서 꺼내온 메시지 Content: {}",message.getContent());
+        log.info("que에서 꺼내온 메시지 RoomId: {}",message.getRoomId());
+        log.info("que에서 꺼내온 메시지 UserId: {}",message.getUserId());
+
+
+        messageService.save(
+            messageService.createMessage(
+                message.getUserId(),
+                message.getRoomId(),
+                message.getContent()));
     }
 }
